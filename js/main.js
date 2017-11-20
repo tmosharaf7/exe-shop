17b64e4d7e54ccf9df0c51d535d9de9dd92720f0
tmosharaf7 <mosharaftanvir7@gmail.com>
2017-11-20 06:38:17 +0600

    product list html updated

--- js/main.js
+++ js/main.js
@@ -86,3 +86,16 @@ $( function() {
 		$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
 			" - $" + $( "#slider-range" ).slider( "values", 1 ) );
 	} );
+
+
+
+
+
+(function(){
+	/*1*/var customSelects = document.querySelectorAll(".custom-dropdown__select");
+	/*2*/for(var i=0; i<customSelects.length; i++){
+		if (customSelects[i].hasAttribute("disabled")){
+			customSelects[i].parentNode.className += " custom-dropdown--disabled";
+		}
+	}	
+})()
